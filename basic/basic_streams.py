1c8cd9e27c86e9da4255ebc8a3578626ac23151e
tommasoguidi <tommaso.guidi.1998@gmail.com>
2025-03-13 11:23:57 +0100

    combsin adesso si fa anche scegliendo le frequenze precise

--- basic/basic_streams.py
+++ basic/basic_streams.py
@@ -41,9 +41,10 @@ class Sin(Stream):
 
 class Square(Stream):
 
-    def __init__(self, freq: float, phase: float, delta: float):
+    def __init__(self, freq: float, ampl: float, phase: float, delta: float):
         super().__init__()
         self.freq = freq
+        self.ampl = ampl
         self.phase = phase
         self.delta = delta
         self.period = 1. / self.freq
@@ -53,7 +54,7 @@ class Square(Stream):
 
     def __getitem__(self, step) -> tuple[torch.Tensor, torch.Tensor] | tuple[None, None]:
         t = step * self.delta + self.phase * self.period
-        y = torch.tensor([[(-1.) ** (math.floor(2. * self.freq * t))]])
+        y = self.ampl * torch.tensor([[(-1.) ** (math.floor(2. * self.freq * t))]])
         d = self.static_d
         return self.adapt_to_attributes(y, d)
 
@@ -61,18 +62,31 @@ class Square(Stream):
 class CombSin(Stream):
     """Synthetic datasets."""
 
-    def __init__(self, f_cap: float, c_cap: float, order: int, delta: float):
+    def __init__(self, f_cap: float | list, c_cap: float | list, order: int, delta: float):
         """Stream obtained by composition of sine waves.
 
-        :param f_cap:       maximum frequency (uniform(0,f_cap)).
-        :param c_cap:       maximum coefficient (uniform(-c_cap,c_cap)).
+        :param f_cap:       maximum frequency (uniform(0,f_cap)) or list of frequencies.
+        :param c_cap:       maximum coefficient (uniform(-c_cap,c_cap)) or list of coefficients.
         :param order:       number of Sine components to sum.
         :param delta:       time (in seconds) between two consecutive elements of the sequence.
         """
         super().__init__()
-        self.freqs = f_cap * torch.rand(order)
+        if isinstance(f_cap, float):
+            self.freqs = f_cap * torch.rand(order)
+        elif isinstance(f_cap, list):
+            self.freqs = torch.tensor(f_cap)
+        else:
+            raise Exception(f"expected float or list for f_cap, not {type(f_cap)}")
         self.phases = math.pi * (2 * torch.rand(order) - 1)
-        self.coeffs = c_cap * (2 * torch.rand(order) - 1)
+        if isinstance(c_cap, float):
+            self.coeffs = c_cap * (2 * torch.rand(order) - 1)
+        elif isinstance(c_cap, list):
+            self.coeffs = torch.tensor(c_cap)
+        else:
+            raise Exception(f"expected float or list for c_cap, not {type(c_cap)}")
+
+        # check all the dimensions
+        assert len(self.coeffs) == len(self.freqs), f"specify the same number of coefficients and frequencies (got {len(self.coeffs)} and {len(self.freqs)} respectively)."
         self.delta = delta
         self.attributes = [Attributes((1,), None),
                            Attributes((1,), [self.name.lower()])]
@@ -84,13 +98,3 @@ class CombSin(Stream):
         y = torch.sum(self.coeffs * torch.sin(2 * math.pi * self.freqs * t + self.phases)).view(1, 1)
         d = self.static_d
         return self.adapt_to_attributes(y, d)
-
-
-class CombSin3(CombSin):
-    def __init__(self, f_cap: float, c_cap: float, delta: float):
-        super().__init__(f_cap, c_cap, 3, delta)
-
-
-class CombSin6(CombSin):
-    def __init__(self, f_cap: float, c_cap: float, delta: float):
-        super().__init__(f_cap, c_cap, 6, delta)
